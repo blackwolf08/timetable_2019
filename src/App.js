7de8537b5d509abd8771c7ba851aa1d55b72831f
Parth Chandna <pchandna24@gmail.com>
2019-08-02 21:37:38 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,29 +9,12 @@ import 'react-perfect-scrollbar/dist/css/styles.css';
 import axios from 'axios';
 class App extends Component {
   state = {
-    data: {}
+    data: {"MON":[{"day":"Monday","type":"Practical","start":"9","time24":9,"subject":"Artificial Intelligence Lab","venue":"BIOINFO","teachers":["SC","P"]},{"day":"Monday","type":"Lecture","start":"11","time24":11,"subject":"BNM","venue":"LT1","teachers":["YG"]}],"TUES":[{"day":"Tuesday","type":"Tutorial","start":"10","time24":10,"subject":"Computer Networks","venue":"TS13","teachers":["K"]},{"day":"Tuesday","type":"Lecture","start":"11","time24":11,"subject":"Principles of Management","venue":"G1","teachers":["PP"]},{"day":"Tuesday","type":"Tutorial","start":"2","time24":14,"subject":"Principles of Mangement","venue":"TS20","teachers":["PP"]},{"day":"Tuesday","type":"Tutorial","start":"4","time24":16,"subject":"BNM","venue":"TS7","teachers":["YG"]}],"WED":[{"day":"Wednesday","type":"Lecture","start":"9","time24":9,"subject":"Artificial Intelligence","venue":"G5","teachers":["PA"]},{"day":"Wednesday","type":"Lecture","start":"10","time24":10,"subject":"Computer Networks","venue":"LT5","teachers":["K"]},{"day":"Wednesday","type":"Lecture","start":"11","time24":11,"subject":"BNM","venue":"LT1","teachers":["YG"]},{"day":"Wednesday","type":"Lecture","start":"1","time24":13,"subject":"Artificial Intelligence","venue":"G1","teachers":["PA"]}],"THUR":[{"day":"Thursday","type":"Tutorial","start":"9","time24":9,"subject":"Artificial Intelligence","venue":"TS19","teachers":["PA"]},{"day":"Thursday","type":"Lecture","start":"10","time24":10,"subject":"Computer Networks","venue":"LT1","teachers":["K"]},{"day":"Thursday","type":"Lecture","start":"11","time24":11,"subject":"Principles of Management","venue":"G1","teachers":["PP"]},{"day":"Thursday","type":"Practical","start":"1","time24":13,"subject":"Computer Networks Lab","venue":"CL2","teachers":["SMT","TAJ","SH"]}],"FRI":[{"day":"Friday","type":"Lecture","start":"9","time24":9,"subject":"Artificial Intelligence","venue":"G2","teachers":["PA"]},{"day":"Friday","type":"Lecture","start":"11","time24":11,"subject":"BNM","venue":"FF8","teachers":["YG"]},{"day":"Friday","type":"Lecture","start":"1","time24":13,"subject":"Computer Networks","venue":"FF7","teachers":["K"]},{"day":"Friday","type":"Practical","start":"3","time24":15,"subject":"Information Security Lab","venue":"CL3","teachers":["APR","APJ","GD"]}],"SAT":[{"day":"Saturday","type":"Practical","start":"9","time24":9,"subject":"Open Source Lab","venue":"CL2","teachers":["AV","DSI","IC","MG"]}]}
   };
   scrollToTop = () => {
     scroll.scrollToTop();
   };
-  componentDidMount() {
-    if (localStorage.getItem('data') && localStorage.getItem('data') !== '') {
-      this.setState({
-        data: JSON.parse(localStorage.getItem('data'))
-      });
-    } else {
-      axios
-        .get(
-          'https://cors-anywhere.herokuapp.com/https://timetablejiit.herokuapp.com/api/data'
-        )
-        .then(res => {
-          console.log(res);
-          this.setState({
-            data: res.data
-          });
-          localStorage.setItem('data', JSON.stringify(res.data));
-        });
-    }
+
   }
   render() {
     return (
